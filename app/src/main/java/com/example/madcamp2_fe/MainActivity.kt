9d717d37acb205dae195b3e58e2c19109226627e
udong1 <dngus1082@gmail.com>
2024-01-05 10:54:35 +0900

    뷰바인딩, 데이터 바인딩을 위해 gradle 파일 수정 메인 화면에 id와 password 입력칸, kakao 연동 버튼 추가

---
+++ app/src/main/java/com/example/madcamp2_fe/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.madcamp2_fe
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
