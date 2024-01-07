654e0da5672b052e5a32ccf822ba1306ee28152b
udong1 <dngus1082@gmail.com>
2024-01-07 21:36:07 +0900

    갤러리에서 사진 가져올 수 있는 기능 추가

--- app/src/main/java/com/example/madcamp2_fe/home/ProfileEditFragment.kt
+++ app/src/main/java/com/example/madcamp2_fe/home/ProfileEditFragment.kt
@@ -10,15 +10,23 @@ import android.os.Environment
 import android.provider.MediaStore
 import android.text.Editable
 import android.text.TextUtils
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.setFragmentResultListener
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.example.madcamp2_fe.R
@@ -32,9 +40,20 @@ class ProfileEditFragment : Fragment() {
     private var _binding : FragmentProfileEditBinding? = null
     private lateinit var walkViewModel : WalkViewModel
     private val binding get() = _binding!!
+    private lateinit var userSelectImg : Uri
+    private lateinit var pickMedia :ActivityResultLauncher<PickVisualMediaRequest>
 
-    private lateinit var getResult : ActivityResultLauncher<Intent>
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setFragmentResultListener("GALLERY_REQUEST"){ _, bundle ->
+            val result:Uri? = bundle.getParcelable("GALLERY_RESULT")
+            userSelectImg = result!!
+            Glide.with(this)
+                .load(result)
+                .into(binding.profile)
+            Log.d("gallery","gallery called with $result")
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -42,13 +61,18 @@ class ProfileEditFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentProfileEditBinding.inflate(inflater, container, false)
         walkViewModel = ViewModelProvider(requireActivity()).get(WalkViewModel::class.java)
+        pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()){ result ->
+            val bundle = Bundle().apply{
+                putParcelable("GALLERY_RESULT", result)
+            }
+            setFragmentResult("GALLERY_REQUEST", bundle)
+        }
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         var nameEdit : String = ""
-        var imgEdit : String = ""
 
         Glide.with(this)
             .load(R.drawable.background)
@@ -67,17 +91,16 @@ class ProfileEditFragment : Fragment() {
         }
         binding.confirm.setOnClickListener{
             //백엔드로 posting
+            //뷰모델 업데이트
             walkViewModel.changeUserName(nameEdit)
-            walkViewModel.setUserProfile(imgEdit)
+            walkViewModel.setUserProfile(userSelectImg.toString())
+            requireActivity().supportFragmentManager.popBackStack()
         }
         binding.imageUpdateButton.setOnClickListener{
             //갤러리로 연결
-            //뷰모델로 연결
-            addFromGallery()
-        }
-    }
-    private fun addFromGallery(){
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
 
+        }
     }
 
 
